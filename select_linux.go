17f1b7a545d9d8186d55e73b8c32a4132dde36fb
rjjrbatarao <50077340+rjjrbatarao@users.noreply.github.com>
2025-12-10 20:35:32 +0800

    Update select_linux.go

--- select_linux.go
+++ select_linux.go
@@ -4,25 +4,14 @@ import (
 	"syscall"
 )
 
-func mySelect(nfd int, readfds *syscall.FdSet, writefds *syscall.FdSet, exceptfds *syscall.FdSet, timeout *syscall.Timeval) (n int, err error) {
-    for {
-        n, err := syscall.Select(nfd, readfds, writefds, exceptfds, timeout)
-        if err == syscall.EINTR {
-            // The system call was interrupted by a signal.
-            // Loop and try again.
-            continue
-        }
-        if err != nil {
-            // A real error occurred.
-            return n, err
-        }
-        // Success (or timeout reached without an error other than EINTR).
-        return n, nil
-    }
-}
 
 func doSelect(nfd int, r *syscall.FdSet, w *syscall.FdSet, e *syscall.FdSet, timeout *syscall.Timeval) (changed bool, err error) {
-	n, err := mySelect(nfd, r, w, e, timeout)
+	n, err := syscall.Select(nfd, r, w, e, timeout)
+    if err == syscall.EINTR {
+            // The system call was interrupted by a signal.
+            // Loop and try again.
+        continue
+    }
 	if err != nil {
 		return false, err
 	}
